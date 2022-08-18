5aa24c38a963e9baae763a15433d1cbfc917e8c7
Singhbarkha2704 <109643730+Singhbarkha2704@users.noreply.github.com>
2022-08-19 01:24:01 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import TicketRaising from './components/TicketRaising';
-
+import Dashboard from './book-store/Dashboard';
+import Header from './book-store/Header';
+import Login from './book-store/Login';
+import NoMatch from './book-store/NoMatch';
+import Signup from './book-store/Signup';
+import {useState} from 'react';
+import Shop from './book-store/Shop';
 function App() {
+ const [isLoggedIn, setisLoggedIn] = useState(false);
+  const email= localStorage.getItem('email');
+  const password= localStorage.getItem('password');
+        if(email==='admin@gmail.com' && password==='admin' )
+        {
+          setisLoggedIn(true)
+        }
   return (
     <div className="App">
-      <TicketRaising/>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path='dashboard' element={ <Dashboard/>}/>
+          <Route path='/shop' element={<Shop/>}/>
+         
+          <Route path='/' element={<Login/>}/>
+          <Route path='/signup' element={<Signup/>} />
+          <Route path='*' element={<NoMatch/>}/>
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
